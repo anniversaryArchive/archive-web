f7aa5ee12870bef6f83f6bc0da128af8b08bb5d1
chochozabe <cintamani_whyk@naver.com>
2022-07-06 10:12:39 +0900

    feat: Recoil, Router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
 import { ApolloProvider } from "@apollo/client";
-import { client } from '../src/apollo/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { client } from "../src/apollo/client";
+import Layout from "./components/layout";
 import "./App.css";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloProvider client={client}>
+        <Layout>
+          <Router>
+            <Routes>
+              <Route path="/" />
+            </Routes>
+          </Router>
+        </Layout>
+      </ApolloProvider>
+    </RecoilRoot>
   );
 }
 
